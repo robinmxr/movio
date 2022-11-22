45a65bfeb2e3e42e6b59c0852035171a5ddd6d4d
deeptoe <rakinfrhn@gmail.com>
2022-11-22 13:37:19 +0600

    abs

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -78,7 +78,7 @@
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
 
-                                <li><a href="route('logout')"
+                                <li><a href="{{ route('logout') }}"
     onclick="event.preventDefault();
     this.closest('form').submit();">
         {{ __('Log Out') }}</a></li>
